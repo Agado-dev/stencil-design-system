5a260bf09a55d7c315ef4162f145ede715178945
Kevin Pennarun <kevin.pennarun@gmail.com>
2025-01-31 15:39:24 +0100

    chore(storybook): change target to esnext

--- packages/agado-components/.storybook/main.ts
+++ packages/agado-components/.storybook/main.ts
@@ -1,12 +1,21 @@
-import { StorybookConfig } from '@storybook/web-components-vite';
+import type { StorybookConfig } from "@storybook/web-components-vite";
 
 const config: StorybookConfig = {
-  stories: ['../src/**/*.stories.@(ts|tsx)'],
-  addons: ['@storybook/addon-essentials'],
-  framework: {
-    name: '@storybook/web-components-vite',
-    options: {},
-  },
-  staticDirs: ['./assets'],
+	stories: ["../src/**/*.stories.@(ts|tsx)"],
+	addons: ["@storybook/addon-essentials"],
+	framework: {
+		name: "@storybook/web-components-vite",
+		options: {},
+	},
+	staticDirs: ["./assets"],
+	viteFinal: (config) => {
+		return {
+			...config,
+			build: {
+				...config.build,
+				target: "esnext",
+			},
+		};
+	},
 };
 export default config;
